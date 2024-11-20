2b521d4a03620d328af980bb3295485220d24550
fbqudrlf09 <fbqudrlf09@naver.com>
2024-11-20 17:54:01 +0900

    Feat Comment, CommentLike 엔티티 생성

---
+++ src/main/java/org/sparta/newsfeed/entity/Comment.java
@@ -0,0 +1,33 @@
+package org.sparta.newsfeed.entity;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Table(name="comment")
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "tinytext")
+    private String comment;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+//    @ManyToOne
+//    @JoinColumn(name = "board_id")
+//    private Board board;
+
+    @OneToMany(mappedBy = "comment",cascade = CascadeType.ALL)
+    @JoinColumn(name = "commentLike_id")
+    private List<CommentLike> commentLike;
+}
