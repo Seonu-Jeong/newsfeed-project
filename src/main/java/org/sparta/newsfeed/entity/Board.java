46b6975dd66b11a300ef00227c3fd64a447fcff3
view0576 <kth9907@gmail.com>
2024-11-20 19:39:52 +0900

    board entity 추가

--- src/main/java/org/sparta/newsfeed/entity/Board.java
+++ src/main/java/org/sparta/newsfeed/entity/Board.java
@@ -1,4 +1,32 @@
 package org.sparta.newsfeed.entity;
 
-public class Board {
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "board")
+
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(columnDefinition = "tinytext")
+    private String postBody;
+
+    @Column(columnDefinition = "tinytext")
+    private String postImage;
+
+    public Board(){
+    }
+
+    public Board(String postBody, String postImage) {
+        this.postBody = postBody;
+        this.postImage = postImage;
+
+
 }
