700abb7ee0cda282f00fe090c400ae7f6ec94241
Seonu-Jeong <sw.jeong97@gmail.com>
2024-11-22 00:39:42 +0900

    Feat : Friend Entity에 생성자 추가

--- src/main/java/org/sparta/newsfeed/entity/Friend.java
+++ src/main/java/org/sparta/newsfeed/entity/Friend.java
@@ -2,8 +2,12 @@ package org.sparta.newsfeed.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.sparta.newsfeed.entity.enums.FriendType;
 
+@Setter
+@NoArgsConstructor
 @Getter
 @Entity
 @Table(name = "friend")
@@ -23,4 +27,10 @@ public class Friend {
 
     @Enumerated(EnumType.STRING)
     private FriendType state;
+
+    public Friend(User postedUser, User requestedUser, FriendType state) {
+        this.postedUser = postedUser;
+        this.requestedUser = requestedUser;
+        this.state = state;
+    }
 }
