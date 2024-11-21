cafeb85476176a2fc805d9e3b0e5a432c7b0cb45
정가현 <kahyun0608@gmail.com>
2024-11-21 14:31:29 +0900

    Style : 줄바꿈 정렬 #18

--- src/main/java/org/sparta/newsfeed/user/dto/SignupRequestDto.java
+++ src/main/java/org/sparta/newsfeed/user/dto/SignupRequestDto.java
@@ -14,7 +14,8 @@ public class SignupRequestDto {
     private String email;
 
     @NotNull
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$", message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
+            message = "비밀번호 형식이 올바르지 않습니다. 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함")
     private String password;
 
     @NotNull
