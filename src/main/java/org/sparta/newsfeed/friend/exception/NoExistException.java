de53affb9860f0272e365463842939cc2aac5407
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-21 11:48:39 +0900

    Add : NoExistException 추가

---
+++ src/main/java/org/sparta/newsfeed/friend/exception/NoExistException.java
@@ -0,0 +1,7 @@
+package org.sparta.newsfeed.friend.exception;
+
+public class NoExistException extends RuntimeException {
+    public NoExistException(String message) {
+        super(message);
+    }
+}
