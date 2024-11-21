f009c977991219e5886db44cfa5058c6b5d50f45
정가현 <kahyun0608@gmail.com>
2024-11-21 22:00:40 +0900

    Feat : nickname 길이제한 추가

--- src/main/java/org/sparta/newsfeed/user/dto/SignupRequestDto.java
+++ src/main/java/org/sparta/newsfeed/user/dto/SignupRequestDto.java
@@ -3,7 +3,7 @@ package org.sparta.newsfeed.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -19,6 +19,7 @@ public class SignupRequestDto {
     private String password;
 
     @NotNull
+    @Size(max=20)
     private String nickname;
 
     public SignupRequestDto(String email, String password, String nickname) {
