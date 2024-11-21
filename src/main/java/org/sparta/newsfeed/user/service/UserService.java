b7905b7492626b69584824b12b870684cb9e1080
정가현 <kahyun0608@gmail.com>
2024-11-21 16:53:50 +0900

    Refactor : UserService 인터페이스화

--- src/main/java/org/sparta/newsfeed/user/service/UserService.java
+++ src/main/java/org/sparta/newsfeed/user/service/UserService.java
@@ -1,39 +1,12 @@
 package org.sparta.newsfeed.user.service;
 
-import lombok.RequiredArgsConstructor;
-import org.sparta.newsfeed.entity.User;
 import org.sparta.newsfeed.user.dto.LoginResponseDto;
 import org.sparta.newsfeed.user.dto.SignupResponseDto;
-import org.sparta.newsfeed.user.repository.UserRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    SignupResponseDto signup (String email, String password, String nickname);
 
-    public SignupResponseDto signup (String email, String password, String nickname) {
-
-        if (userRepository.existsByEmail(email)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하는 이메일입니다.");
-        }
-
-        User user = new User(email, password, nickname);
-
-        User savedUser = userRepository.save(user);
-
-        return new SignupResponseDto(savedUser.getEmail(), savedUser.getNickname());
-
-    }
-
-    public LoginResponseDto login (String email, String password) {
-
-        User loginUser = userRepository.findByEmailAndPasswordOrElse(email, password);
-
-        return new LoginResponseDto(loginUser.getId());
-    }
+    LoginResponseDto login(String email, String password);
 
 }
