9c22cce5d28f8df19ac45e538f9f9c2a85af6183
정가현 <kahyun0608@gmail.com>
2024-11-22 12:06:29 +0900

    Feat : 회원가입 시 탈퇴 이메일 예외처리

--- src/main/java/org/sparta/newsfeed/user/service/UserServiceImpl.java
+++ src/main/java/org/sparta/newsfeed/user/service/UserServiceImpl.java
@@ -21,7 +21,7 @@ public class UserServiceImpl implements UserService {
 
     public SignupResponseDto signup(SignupRequestDto requestDto) {
 
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
+        if (userRepository.countByEmail(requestDto.getEmail()) > 0) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하는 이메일입니다.");
         }
 
@@ -63,17 +63,23 @@ public class UserServiceImpl implements UserService {
         user.setUserImage(requestDto.getUserImage());
         user.setSelfComment(requestDto.getSelfComment());
 
-        //비밀번호 수정 -> oldPassword, newPassword 는 둘다 입력될 경우에만 수정
-        if (requestDto.getOldPassword() != null && requestDto.getNewPassword() != null) {
-            //기존 비밀번호와 새 비밀번호 비교
-            isSamePassword(requestDto);
+        if (requestDto.getOldPassword() == null && requestDto.getNewPassword() == null) {
+
+            return new UserResponseDto(user);
+        }
+
+        else if (requestDto.getOldPassword() != null && requestDto.getNewPassword() != null) {
+
+            isSamePassword(requestDto);     //기존 비밀번호와 새 비밀번호 비교
+
             //oldPassword 를 데이터의 password 와 비교한 후 newPassword 를 encode 하여 변경
             if (passwordEncoder.matches(requestDto.getOldPassword(), user.getPassword())) {
+
                 user.setPassword(passwordEncoder.encode(requestDto.getNewPassword()));
             } else {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
             }
-        } else if (requestDto.getOldPassword() == null && requestDto.getNewPassword() == null) {
+
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
